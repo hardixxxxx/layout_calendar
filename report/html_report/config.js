6ee1c13c7c7d39dd2f9ab766481532f8d80ba8b2
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-15 13:18:40 +0100

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,113 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_initial_state.png",
+        "test": "..\\bitmaps_test\\20250214-223605\\Calendar_initial_state.png",
+        "selector": "body",
+        "fileName": "Calendar_initial_state.png",
+        "label": "Calendar initial state",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 18.922297159830727,
+          "misMatchPercentage": "18.92",
+          "analysisTime": 25
+        },
+        "diffImage": "..\\bitmaps_test\\20250214-223605\\failed_diff_Calendar_initial_state.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_day_with_hover_effect.png",
+        "test": "..\\bitmaps_test\\20250214-223605\\Calendar_day_with_hover_effect.png",
+        "selector": "body",
+        "fileName": "Calendar_day_with_hover_effect.png",
+        "label": "Calendar day with hover effect",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 20.077006022135414,
+          "misMatchPercentage": "20.08",
+          "analysisTime": 25
+        },
+        "diffImage": "..\\bitmaps_test\\20250214-223605\\failed_diff_Calendar_day_with_hover_effect.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_starting_from_Wednesday.png",
+        "test": "..\\bitmaps_test\\20250214-223605\\Calendar_starting_from_Wednesday.png",
+        "selector": "body",
+        "fileName": "Calendar_starting_from_Wednesday.png",
+        "label": "Calendar starting from Wednesday",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 26.826222737630207,
+          "misMatchPercentage": "26.83",
+          "analysisTime": 21
+        },
+        "diffImage": "..\\bitmaps_test\\20250214-223605\\failed_diff_Calendar_starting_from_Wednesday.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Calendar_with_length_of_29_days.png",
+        "test": "..\\bitmaps_test\\20250214-223605\\Calendar_with_length_of_29_days.png",
+        "selector": "body",
+        "fileName": "Calendar_with_length_of_29_days.png",
+        "label": "Calendar with length of 29 days",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.5,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/calendar/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 26.26355489095052,
+          "misMatchPercentage": "26.26",
+          "analysisTime": 25
+        },
+        "diffImage": "..\\bitmaps_test\\20250214-223605\\failed_diff_Calendar_with_length_of_29_days.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
